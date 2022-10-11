32bc9e206b2a04790b4bad2239c922b21dead585
leesuyeeeong <1913839@sookmyung.ac.kr>
2022-10-11 17:12:07 +0900

    실습4 call by value

--- main.c
+++ main.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
-void func(int x) {
-	
-	printf("func x is at %p\n", &x);
+/*int square( int a )
+{
+	return (a *a);
 }
-
-int main(void) {
-	int x;
-	printf("main x is at %p\n", &x);
-	func(x);
-	
-	return 0;
+int main()
+{
+	int a = 2;
+	a = square(a);
+	printf("a=%i\n",a);
 }
-//함수 실행시 메모리 상태 관찰 
-
+*/
+void square( int a )
+{
+	a = a *a;
+}
+int main()
+{
+	int a = 2;
+	square(a);
+	printf("a=%i\n",a);
+}
