f6970d953729cc0ecb7f0eda83ef5646eca4ecb7
leesuyeeeong <1913839@sookmyung.ac.kr>
2022-10-12 12:54:19 +0900

    실습5 combination

--- main.c
+++ main.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
+#include<string.h>
 
-/*int square( int a )
-{
-	return (a *a);
+long long fact(int num) {
+	if (num == 1 || num == 0) {
+		return 1;
+	}
+	return num * fact(num - 1);
 }
-int main()
-{
-	int a = 2;
-	a = square(a);
-	printf("a=%i\n",a);
+int combi(int n, int r) {
+	if (n == r) {
+		return 1;
+	}
+	int a = fact(n);
+	int b = fact(r);
+	int c = fact(abs(n - r));
+	return a / (b*c);
 }
-*/
-void square( int a )
-{
-	a = a *a;
+int main() {
+	int n, r, res;
+
+	printf("두 정수를 입력하시오: ");
+	scanf("%d %d", &n, &r);
+
+	res = combi(n, r);
+	printf("c(%d, %d) = %d", n, r, res);
+
+
+	return 0;
 }
-int main()
-{
-	int a = 2;
-	square(a);
-	printf("a=%i\n",a);
-}
+
